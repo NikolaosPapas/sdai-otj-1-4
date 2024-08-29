e72ef82d8b5b8a134b54c1398a5b2450a79a9ae7
Nikolaos Papas <116779095+NikolaosPapas@users.noreply.github.com>
2024-08-29 03:49:58 +0000

    updated h2 and h4 css details

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log("My java works")
+console.log("My java works")// to see this open the inspector than console. also remember to fix the images 
+
+
+//Function to calculate the area of a rectangle
+//Definition of the function
+function areaRectangle (legnth, width) {
+    return legnth * width
+}
+
+//call function to use
+console.log (areaRectangle(10, 20))
+
+
+//challenge
+function divide (num1, num2) {
+    return num1/num2
+}
+
+console.log (divide(10,20))
+console.log (divide(120,230))
+console.log (divide(20,30))
+
+
+
+
+//challenge 2
